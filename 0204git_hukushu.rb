4b031ceface3c821d0c7421b5d20c8799c9c2871
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2019-02-04 23:48:54 +0900

    add_score

--- 0204git_hukushu.rb
+++ 0204git_hukushu.rb
@@ -6,3 +6,14 @@ skills.each do |skill|
 end
 
 skills
+
+
+score = gets.chomp.to_i
+
+if score >= 3
+  puts "私のチームの勝ちです"
+elsif score > 0 && score <= 2
+  puts "いい試合をしました"
+else
+  puts "残念でした"
+end
